c850238ab5482faa0a1c4bb3a7ce5fd019bf947d
Kingidy <aadityachawda0908@gmail.com>
2025-05-05 16:27:16 +0700

    xxc

---
+++ Identifiers.py
@@ -0,0 +1,12 @@
+# Creating Diffrent Variables
+x = 5
+y =" John "
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+print("\nHello", name, "\nwelcome to codingal")
